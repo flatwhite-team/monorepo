c2deb6a102487a59b2a90ad982eae9aa46accd2e
jaehoo <im.jaehoo1@gmail.com>
2023-10-29 19:22:55 +0900

    prisma package

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { api } from "~/utils/api";
+
 export default function HomePage() {
+  const { data } = api.store.findFirst.useQuery();
+  console.log(data);
+
   return (
     <main className="flex h-screen flex-col items-center">
       <h1>Hi</h1>
