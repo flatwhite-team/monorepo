70ec90099869684971d19778be8f2a10f39858c5
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2023-11-02 01:10:08 +0900

    feat: nativewind config & icons (#11)

--- apps/app-frontend/src/screens/StoreDetailScreen/components/InfoTabContent.tsx
+++ apps/app-frontend/src/screens/StoreDetailScreen/components/InfoTabContent.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ReactNode } from "react";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { RouteProp, useRoute } from "@react-navigation/native";
 
 import { BusinessDay } from "../../../models/BusinessDay";
@@ -16,41 +18,41 @@ export function InfoTabContent() {
   }
 
   return (
-    <View style={Style.storeInfoSection}>
-      <View style={Style.infoText}>
-        {store.businessDays.length > 0 ? (
-          BusinessDay.sort(store.businessDays).map((businessDay) => {
-            return (
-              <Text key={businessDay.dayOfWeek} style={Style.subText}>
-                {BusinessDay.formatBusinessDay(businessDay)}
-              </Text>
-            );
-          })
-        ) : (
-          <Text style={Style.subText}>가게 문의</Text>
-        )}
+    <View className="my-5 flex flex-col">
+      <View className="mb-2 flex flex-row gap-x-2">
+        <View className="mt-0.5">
+          <Ionicons name="time-outline" size={18} />
+        </View>
+        <View>
+          {store.businessDays.length > 0 ? (
+            BusinessDay.sort(store.businessDays).map((businessDay) => {
+              return (
+                <Text key={businessDay.dayOfWeek} className="text-base">
+                  {BusinessDay.formatBusinessDay(businessDay)}
+                </Text>
+              );
+            })
+          ) : (
+            <Text className="text-base">가게 문의</Text>
+          )}
+        </View>
       </View>
-      <View style={Style.infoText}>
-        <Text style={Style.subText}>{store.tel}</Text>
-      </View>
+      <ContentWrapper>
+        <Ionicons name="call-outline" size={18} />
+        <Text className="text-base">{store.tel}</Text>
+      </ContentWrapper>
+      <ContentWrapper>
+        <Ionicons name="map-outline" size={18} />
+        <Text className="text-base">{store.address}</Text>
+      </ContentWrapper>
     </View>
   );
 }
 
-const Style = StyleSheet.create({
-  storeInfoSection: {
-    marginTop: 20,
-    marginBottom: 20,
-    display: "flex",
-    flexDirection: "column",
-    gap: 8,
-  },
-  infoText: {
-    display: "flex",
-    flexDirection: "column",
-    gap: 4,
-  },
-  subText: {
-    fontSize: 16,
-  },
-});
+function ContentWrapper({ children }: { children: ReactNode }) {
+  return (
+    <View className="not-last:mb-0 mb-2 flex flex-row items-center gap-x-2">
+      {children}
+    </View>
+  );
+}
