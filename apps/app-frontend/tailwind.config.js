b0f0170cacddeb0d86a0ce93b201b2e0af31ba63
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2023-11-07 03:18:12 +0900

    fix: colors (#22)

--- apps/app-frontend/tailwind.config.js
+++ apps/app-frontend/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#2F68A8",
-      background: "#FDFCF9",
+    extend: {
+      colors: {
+        primary: "#2F68A8",
+      },
     },
   },
   plugins: [],
