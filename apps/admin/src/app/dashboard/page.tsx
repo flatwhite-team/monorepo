249dd7e6473493c1c44bbcd8f63193b211fb891a
jaehoo <im.jaehoo1@gmail.com>
2023-11-24 01:53:56 +0900

    test deploy

--- apps/admin/src/app/dashboard/page.tsx
+++ apps/admin/src/app/dashboard/page.tsx
@@ -8,6 +8,15 @@ import { useAuthorizedSession } from "../_providers/AuthorizedSessionProvider";
 export default function DashboardHome() {
   const { user } = useAuthorizedSession();
   const { data: stores } = api.store.findManagingByUserId.useQuery(user.id);
+  const { data } = api.store.infiniteFindByDistance.useQuery({
+    locationOptions: {
+      latitude: 37.505441,
+      longitude: 127.036701,
+      radius: 1000,
+    },
+    filters: [["HAND_DRIP"]],
+  });
+  console.log(data);
 
   assert(stores != null, "상점 정보가 없습니다.");
 
