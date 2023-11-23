293d43bf354723626af6e01ad8453db4d7314561
jaehoo <im.jaehoo1@gmail.com>
2023-11-24 02:09:10 +0900

    테스트용 커밋 내용 제거

--- apps/admin/src/app/dashboard/page.tsx
+++ apps/admin/src/app/dashboard/page.tsx
@@ -8,15 +8,6 @@ import { useAuthorizedSession } from "../_providers/AuthorizedSessionProvider";
 export default function DashboardHome() {
   const { user } = useAuthorizedSession();
   const { data: stores } = api.store.findManagingByUserId.useQuery(user.id);
-  const { data } = api.store.infiniteFindByDistance.useQuery({
-    locationOptions: {
-      latitude: 37.505441,
-      longitude: 127.036701,
-      radius: 1000,
-    },
-    filters: [["HAND_DRIP"]],
-  });
-  console.log(data);
 
   assert(stores != null, "상점 정보가 없습니다.");
 
@@ -24,13 +15,6 @@ export default function DashboardHome() {
     return (
       <div key={id}>
         <p>{name}</p>
-        <button
-          onClick={() => {
-            console.log(id);
-          }}
-        >
-          이동
-        </button>
       </div>
     );
   });
