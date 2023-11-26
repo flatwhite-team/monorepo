34eca5dfe103678db5bd99bf16999afd8bd1adda
jaehoo <im.jaehoo1@gmail.com>
2023-11-27 00:09:14 +0900

    feat: 상세화면 고도화 - 주소 복사, 지도

--- apps/app-frontend/src/screens/StoreDetailScreen/components/InfoTabContent.tsx
+++ apps/app-frontend/src/screens/StoreDetailScreen/components/InfoTabContent.tsx
@@ -1,14 +1,27 @@
-import { ReactNode } from "react";
+import { Suspense } from "react";
 import { ScrollView, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { setStringAsync } from "expo-clipboard";
 import { Ionicons } from "@expo/vector-icons";
 import { RouteProp, useRoute } from "@react-navigation/native";
+import { XStack, YStack } from "tamagui";
 
+import { CenteredActivityIndicator } from "~/components/CenteredActivityIndicator";
 import { colors } from "~/constants";
-import { BusinessDay } from "../../../models/BusinessDay";
-import { HomeStackParamList } from "../../../navigation/HomeStackNavigator";
-import { api } from "../../../utils/api";
+import { BusinessDay } from "~/models/BusinessDay";
+import { HomeStackParamList } from "~/navigation/HomeStackNavigator";
+import { api } from "~/utils/api";
 
 export function InfoTabContent() {
+  return (
+    <Suspense fallback={<CenteredActivityIndicator size="large" />}>
+      <Resolved />
+    </Suspense>
+  );
+}
+
+function Resolved() {
   const {
     params: { storeId },
   } = useRoute<RouteProp<HomeStackParamList, "StoreDetailScreen">>();
@@ -19,56 +32,102 @@ export function InfoTabContent() {
   }
 
   return (
-    <ScrollView className="mb-5 flex flex-1 flex-col px-5 pt-5">
-      <View className="mb-2 flex flex-row gap-x-2">
-        <View className="mt-0.5">
-          <Ionicons name="time-outline" size={20} color={colors.gray900} />
-        </View>
-        <View>
-          {store.businessDays.length > 0 ? (
-            BusinessDay.sort(store.businessDays).map((businessDay) => {
-              return (
-                <Text
-                  key={businessDay.dayOfWeek}
-                  className="text-base text-gray-900"
-                >
-                  {BusinessDay.formatBusinessDay(businessDay)}
-                </Text>
-              );
-            })
-          ) : (
-            <Text className="text-base text-gray-900">가게 문의</Text>
-          )}
-        </View>
-      </View>
-      <ContentWrapper>
-        <Ionicons name="call-outline" size={20} color={colors.gray900} />
-        <Text className="text-base text-gray-900">{store.phoneNumber}</Text>
-      </ContentWrapper>
-      <ContentWrapper>
-        <Ionicons name="map-outline" size={20} color={colors.gray900} />
-        <Text className="text-base text-gray-900">{store.address}</Text>
-      </ContentWrapper>
-      {store.description != null ? (
-        <View className="mb-5 flex flex-row gap-x-2">
+    <ScrollView
+      contentContainerStyle={{
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        paddingBottom: 48,
+      }}
+    >
+      <YStack gap={8}>
+        <XStack gap={8}>
           <View className="mt-0.5">
-            <Ionicons
-              name="information-circle-outline"
-              size={20}
-              color={colors.gray900}
-            />
+            <Ionicons name="time-outline" size={20} color={colors.gray900} />
           </View>
-          <Text className="flex-1 text-base text-gray-900">{`${store.description}`}</Text>
-        </View>
-      ) : null}
+          <View>
+            {store.businessDays.length > 0 ? (
+              BusinessDay.sort(store.businessDays).map((businessDay) => {
+                return (
+                  <Text
+                    key={businessDay.dayOfWeek}
+                    className="text-base text-gray-900"
+                  >
+                    {BusinessDay.formatBusinessDay(businessDay)}
+                  </Text>
+                );
+              })
+            ) : (
+              <Text className="text-base text-gray-900">가게 문의</Text>
+            )}
+          </View>
+        </XStack>
+        <XStack gap={8} className="items-center">
+          <Ionicons name="call-outline" size={20} color={colors.gray900} />
+          <Text className="text-base text-gray-900">{store.phoneNumber}</Text>
+        </XStack>
+        {store.description != null ? (
+          <XStack gap={8}>
+            <View className="mt-0.5">
+              <Ionicons
+                name="information-circle-outline"
+                size={20}
+                color={colors.gray900}
+              />
+            </View>
+            <Text className="flex-1 text-base text-gray-900">
+              {store.description}
+            </Text>
+          </XStack>
+        ) : null}
+        <XStack gap={8}>
+          <View className="mt-0.5">
+            <Ionicons name="map-outline" size={20} color={colors.gray900} />
+          </View>
+          <YStack gap={8} className="flex-1">
+            <TouchableOpacity
+              onPress={() => {
+                setStringAsync(store.address);
+              }}
+            >
+              <Text className="flex-1 text-base text-gray-900">
+                {store.address + " "}
+                <Ionicons
+                  name="copy-outline"
+                  size={18}
+                  color={colors.blue500}
+                />
+              </Text>
+            </TouchableOpacity>
+            <View className="h-44">
+              <MapView
+                className="w-full flex-1"
+                provider={PROVIDER_GOOGLE}
+                userLocationPriority="balanced"
+                showsUserLocation={true}
+                initialCamera={{
+                  center: {
+                    latitude: store.latitude,
+                    longitude: store.longitude,
+                  },
+                  heading: 0,
+                  pitch: 0,
+                  zoom: 15,
+                }}
+                rotateEnabled={false}
+                pitchEnabled={false}
+                toolbarEnabled={false}
+              >
+                <Marker
+                  coordinate={{
+                    latitude: store.latitude,
+                    longitude: store.longitude,
+                  }}
+                />
+              </MapView>
+            </View>
+          </YStack>
+        </XStack>
+      </YStack>
     </ScrollView>
-  );
-}
-
-function ContentWrapper({ children }: { children: ReactNode }) {
-  return (
-    <View className="not-last:mb-0 mb-2 flex flex-row items-center gap-x-2">
-      {children}
-    </View>
   );
 }
