3e9a004ae2b84fe0fc67868292556976f52f9d9d
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2023-12-10 03:19:32 +0900

    feat: send noti message from app (#48)

--- packages/trpc-server/src/root.ts
+++ packages/trpc-server/src/root.ts
@@ -1,12 +1,10 @@
 import { menuRouter } from "./router/menu";
-import { slackRouter } from "./router/slack";
 import { storeRouter } from "./router/store";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   store: storeRouter,
   menu: menuRouter,
-  slack: slackRouter,
 });
 
 // export type definition of API
