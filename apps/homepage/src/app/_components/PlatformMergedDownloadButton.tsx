e8774318d767e950f9f8ce627e471878a679c06b
jaehoo <im.jaehoo1@gmail.com>
2024-02-09 23:56:46 +0900

    feat: add logs

--- apps/homepage/src/app/_components/PlatformMergedDownloadButton.tsx
+++ apps/homepage/src/app/_components/PlatformMergedDownloadButton.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import { useEffect } from "react";
+
+import { sendSlackNotibotMessage } from "~/utils/sendSlackNotibotMessage";
+
 export function PlatformMergedDownloadButton() {
   "use client";
+
+  useEffect(() => {
+    sendSlackNotibotMessage(`앱 다운로드 버튼 노출\n${navigator.userAgent}`);
+  }, []);
 
   return (
     <div className="fixed bottom-12 left-0 right-0 flex justify-center p-4">
       <button
         className="bg-primary w-52 rounded-lg p-5 text-2xl font-bold text-white shadow-2xl"
         onClick={() => {
+          sendSlackNotibotMessage("앱 다운로드 버튼 클릭");
+
           if (isIos() || isMac()) {
             return window.open(appStoreUrl, "_blank");
           }
