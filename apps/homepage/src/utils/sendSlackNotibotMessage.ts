920106d83eb185886b12ef4355a7eccf1c144d5d
jaehoo <im.jaehoo1@gmail.com>
2024-02-11 15:41:19 +0900

    feat: console log slack message in local browser

--- apps/homepage/src/utils/sendSlackNotibotMessage.ts
+++ apps/homepage/src/utils/sendSlackNotibotMessage.ts
@@ -1,4 +1,10 @@
 export function sendSlackNotibotMessage(input: string) {
+  if (isLocalhostBrowser()) {
+    console.log("[slack]", input);
+
+    return;
+  }
+
   return fetch(
     "https://hooks.slack.com/services/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",
     {
@@ -7,5 +13,13 @@ export function sendSlackNotibotMessage(input: string) {
         text: input,
       }),
     },
+  );
+}
+
+function isLocalhostBrowser() {
+  return (
+    typeof window !== "undefined" &&
+    (window.location.hostname === "localhost" ||
+      window.location.hostname === "127.0.0.1")
   );
 }
